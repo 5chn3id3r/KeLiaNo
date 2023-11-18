afab02c7f6e65458f2e17e1fb7008266342d2280
arsendier <schneider_ad@protonmail.com>
2023-11-18 18:44:33 +0100

    FIX

--- lib/widgets/accueil.dart
+++ lib/widgets/accueil.dart
@@ -2,22 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
 
-void makeRequest() async{
-  var response = await http.get(Uri.parse('https://www.fdj.fr/jeux-de-tirage/keno/resultats'));
-  //If the http request is successful the statusCode will be 200
-  if(response.statusCode == 200){
-    String htmlToParse = response.body;
-    print(htmlToParse);
-  }
-}
-
-
 class Accueil extends StatelessWidget {
-
   List<String> values = [];
 
-  Accueil()
-  {
+  Accueil() {
     scrapPage();
   }
 
@@ -30,22 +18,22 @@ class Accueil extends StatelessWidget {
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-          children: [
-            Text(
-              'Bienvenue sur l\'application Keno!',
-              style: TextStyle(fontSize: 24),
-            ),
-            ListView(
+      child: Column(
+        children: [
+          Text(
+            'Bienvenue sur l\'application Keno!',
+            style: TextStyle(fontSize: 24),
+          ),
+          Expanded(  // Ajout d'un widget Expanded
+            child: ListView(
               children: values.map((element) => Text(element)).toList(),
-            )
-          ],
-        )
-      );
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
